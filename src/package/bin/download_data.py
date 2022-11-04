1e805e51a0fa372374dd629add83bb949094c0bf
bautt <tbaublys@gmail.com>
2022-11-04 20:25:43 +0100

    input script
    Former-commit-id: d788b682076a573a2fa433ffe55fa328b162d504

--- src/package/bin/download_data.py
+++ src/package/bin/download_data.py
@@ -1,5 +1,11 @@
 #!/usr/bin/env python3
-
+from datetime import datetime
 import urllib.request
+
 url = 'https://github.com/bautt/splunk4champions/raw/master/splunk4champions/static/current.log.gz'
-urllib.request.urlretrieve(url, '../static/current.tar.gz')
+#urllib.request.urlretrieve(url, '../static/current.tar.gz')
+now = datetime.now()
+
+message=(" Splunk4Champions download script for weather data was started")
+dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+print(dt_string + message)
