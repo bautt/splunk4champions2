2dab4432bd7314f975f5c978b174dd7864e0444a
bautt <tbaublys@gmail.com>
2022-11-06 22:35:44 +0100

    data upload script
    Former-commit-id: 19ed12b29531066ad830679161c6083220e7431b

--- src/package/bin/download_data.py
+++ src/package/bin/download_data.py
@@ -1,9 +1,12 @@
-#!/usr/bin/env python3
 from datetime import datetime
 import urllib.request
 
 url = 'https://github.com/bautt/splunk4champions/raw/master/splunk4champions/static/current.log.gz'
-#urllib.request.urlretrieve(url, '../static/current.tar.gz')
+target = '/opt/splunk/etc/apps/splunk4champions2/static/current.log.gz'
+#target = '../static/current.log.gz'
+#target = '$SPLUNK_HOME/etc/apps/splunk4champions2/static/current.log.gz'
+
+urllib.request.urlretrieve(url, target)
 now = datetime.now()
 
 message=(" Splunk4Champions download script for weather data was started")
