3d1fd07da22f7ab0e191441d85e925d9c8f3b8c7
bautt <tbaublys@gmail.com>
2022-11-07 10:38:08 +0100

    2.0.6
    Former-commit-id: 206b9085292638225453de5b3c0469d344d685cb

--- src/package/bin/download_data.py
+++ src/package/bin/download_data.py
@@ -3,7 +3,9 @@
 import datetime 
 import time 
 import urllib.request
+import os 
 
+print(os.environ['SPLUNK_HOME'])
 start = time.time()
 url = 'https://github.com/bautt/splunk4champions/raw/master/splunk4champions/static/current.log.gz'
 target = '/opt/splunk/etc/apps/splunk4champions2/static/current.log.gz'
@@ -13,7 +15,7 @@ target = '/opt/splunk/etc/apps/splunk4champions2/static/current.log.gz'
 d = urllib.request.urlopen(url)
 # print(d.info())
 content_length = (d.info()['Content-Length'])
-urllib.request.urlretrieve(url, target)
+#urllib.request.urlretrieve(url, target)
 
 
 dauer = ('{0:0.1f}'.format(time.time() - start))
