1702a468d4076f7c38ddd3a1874c1d46e07a5df4
bautt <tbaublys@gmail.com>
2022-11-08 14:17:01 +0100

    remove duplicate
    Former-commit-id: 28a4c8d6337a414bc72e53f1124d0b462fa5195b

--- src/package/bin/download_data.py
+++ src/package/bin/download_data.py
@@ -4,24 +4,32 @@ import datetime
 import time 
 import urllib.request
 import os 
+from pathlib import Path
 
-print(os.environ['SPLUNK_HOME'])
+splunkhome = (os.environ['SPLUNK_HOME'])
+
+mydir = '{PWD}'.format(**os.environ)
+path = Path(Path.cwd())
+parent = path.parent
 start = time.time()
 url = 'https://github.com/bautt/splunk4champions/raw/master/splunk4champions/static/current.log.gz'
-target = '/opt/splunk/etc/apps/splunk4champions2/static/current.log.gz'
-#target = '../static/current.log.gz'
-#target = '$SPLUNK_HOME/etc/apps/splunk4champions2/static/current.log.gz'
+target = (splunkhome + '/etc/apps/splunk4champions2/static/current.log.gz')
 
+# getting size info from the file
 d = urllib.request.urlopen(url)
 # print(d.info())
 content_length = (d.info()['Content-Length'])
-#urllib.request.urlretrieve(url, target)
+
+# this is downloading 
+urllib.request.urlretrieve(url, target)
 
 
 dauer = ('{0:0.1f}'.format(time.time() - start))
 now = datetime.datetime.now()
 datum = (now.strftime("%Y-%m-%d %H:%M:%S"))
 message2=(" Splunk4Champions2 download script for weather data was finished in ")
-print(datum +  message2 + str(dauer) + ' seconds. The size of the file was ' + str(content_length) + ' Bytes online')
-
-
+print(datum + message2 + str(dauer) + ' seconds. The size of the file was ' + str(content_length) + ' Bytes online')
+#print(mydir)
+#print(splunkhome)
+#print(path.parent)
+#print(target)
