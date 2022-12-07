f44c19dce6944abe65a9b5949294215acf234cdc
bautt <tbaublys@gmail.com>
2022-12-07 17:19:35 +0100

    test ohne data
    Former-commit-id: a05b0666c6305318babf4bd5e62263c1ea547fd4

--- s4c_getweather.py
+++
@@ -1,8 +0,0 @@
-import tarfile
-def reset(tarinfo):
-        tarinfo.uid = tarinfo.gid = 0
-        tarinfo.uname = tarinfo.gname = "root"
-        return tarinfo
-tar = tarfile.open("current.log.gz", "w:gz")
-tar.add("/var/log/current.log", filter=reset)
-tar.close()
